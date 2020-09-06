d389e891c5768b475fdf4c67a0bad2e7a1e656c2
vikrantRajan <vikrantrajan93@gmail.com>
2020-09-06 10:10:57 -0700

    added regex

--- includes/db.php
+++ includes/db.php
@@ -10,12 +10,8 @@ Class ConnectToDB {
     
         static function con() {
             
-<<<<<<< HEAD
-            $conDetails = parse_ini_file("/var/www/entguide/entguide.ini");
-=======
             // $conDetails = parse_ini_file("/var/www/entguide/entguide.ini");
             $conDetails = parse_ini_file("../../../entguide/entguide.ini");
->>>>>>> 15ab65f87f17bcc9d59e7e8c63a0a1ca6faf13cb
           
             $connect = mysqli_connect( $conDetails['server'], $conDetails['user'], $conDetails['pass'], $conDetails['db'] ); 
             
